fc422c05fcd96508a69b94720ca7dc431248893d
MattLeuterio <leuterio.matteo@gmail.com>
2021-01-06 02:17:13 +0100

    manifest and metadataù

--- src/components/GlobalLayout/index.jsx
+++ src/components/GlobalLayout/index.jsx
@@ -5,38 +5,43 @@ import { Menu } from "../index";
 import { PageContainer, Background, Cookie, Text } from "./style";
 import { withMediaQueries } from "../../hoc/withMediaQueries";
 import { Button, SocialsRow } from "../../atoms";
-import { BgR6, BgCod, BgCounterStrike, BgValorant } from "../../ui/assets/img/backgrounds"
+import {
+  BgR6,
+  BgCod,
+  BgCounterStrike,
+  BgValorant,
+} from "../../ui/assets/img/backgrounds";
 import Inter from "../../ui/typography/inter";
 
 const GlobalLayout = ({ children, mediaIsPhone }) => {
   const [cookies, setCookie] = useCookies(["user"]);
-  const bgs = [ BgR6, BgCod, BgCounterStrike, BgValorant ];
+  const bgs = [BgR6, BgCod, BgCounterStrike, BgValorant];
 
   const handleCookie = () => {
     setCookie("user", "accept", {
-      path: "/"
-    })
-    console.log(cookies)
+      path: "/",
+    });
   };
-
-  console.log(cookies);
 
   return (
     <>
-      <Background bgs={bgs[Math.floor(Math.random() * 4)]}/>
+      <Background bgs={bgs[Math.floor(Math.random() * 4)]} />
       {!cookies.user && (
-      <Cookie>
-        <Text>
-          <Inter type="cardNotifyDescription">
-            We use cookies and other tracking technologies to improve your browsing experience on our website, to show you personalized content and targeted ads, to analyze our website traffic, and to understand where our visitors are coming from.
-          </Inter>
-        </Text>
-        <Button 
-          text="Accept"
-          onClick={() => handleCookie()} 
-          padding="10px 70px"
+        <Cookie>
+          <Text>
+            <Inter type="cardNotifyDescription">
+              We use cookies and other tracking technologies to improve your
+              browsing experience on our website, to show you personalized
+              content and targeted ads, to analyze our website traffic, and to
+              understand where our visitors are coming from.
+            </Inter>
+          </Text>
+          <Button
+            text="Accept"
+            onClick={() => handleCookie()}
+            padding="10px 70px"
           />
-      </Cookie>
+        </Cookie>
       )}
       <Menu />
       {!mediaIsPhone && <SocialsRow />}
